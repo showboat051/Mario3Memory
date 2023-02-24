85b3f90814f790ee32a29b9935b874cb0b328394
showboat051 <rashad.davis87@yahoo.com>
2023-02-23 23:00:22 -0600

    general work

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const cards = document.querySelectorAll('.card');
 
 
 
-// BUTTON CLICKS
-for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function() {
-        console.log("clickeddd");
-    })
+// // BUTTON CLICKS
+// for (var i = 0; i < cards.length; i++) {
+//     cards[i].addEventListener("click", function() {
+//         console.log("clickeddd");
+//     })
+// }
+
+// Flip Card
+function flipCard() {
+    this.classList.toggle('flip')
 }
+
+cards.forEach(card => card.addEventListener('click', flipCard));
