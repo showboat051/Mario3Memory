8ccc134c5d7bec1cbd2617fc86890e63c07c86d7
showboat051 <rashad.davis87@yahoo.com>
2023-03-30 01:15:13 -0500

    picking up from scratch

--- script.js
+++ script.js
@@ -15,18 +15,31 @@ const cardArray = [
    }
 ]
 
+let firstCard , secondCard
+let hasFlippedCard = false;
+
 
 
 
 // Flip Card
 function flipCard() {
-    this.classList.toggle('flip')
-}
+    this.classList.add('flip')
+
+    if(!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = true;
+      return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+} // flip card()
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-// // Hide the card on click
-// function hideCard () {
-//    this.classList.style.display = "none";
+// Hide the card on click
+function hideCard (value) {
+   document.getElementById(value).style.display = "none";
   
-// }
+}
