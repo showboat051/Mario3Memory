002dd30ea0491f4e6162d41922ed119584fca2d4
showboat051 <rashad.davis87@yahoo.com>
2023-04-08 15:25:08 -0500

    starting makeGameBoard()

---
+++ script2.js
@@ -0,0 +1,21 @@
+//**********************UNIT RESOURCES************************************** */
+const start = document.getElementById('startButton');
+const gameBoard = document.getElementById('gameBoard'); 
+
+
+
+
+
+// Start Game
+function startGame() {
+
+    makeGameBoard(3,3);
+} // startGame()
+
+
+
+
+// Make gameboard
+function makeGameBoard(rows, columns) {
+
+} // makeGameBoard()
